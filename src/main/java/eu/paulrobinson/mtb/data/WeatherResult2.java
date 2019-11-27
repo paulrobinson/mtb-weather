6c0e11c0f6a162afe22e06a63d77e97bf49656d6
Paul Robinson <paul.robinson@redhat.com>
2019-11-27 12:40:05 +0000

    Unified data service collector gathers past ad future weather

--- src/main/java/eu/paulrobinson/mtb/data/WeatherResult2.java
+++ src/main/java/eu/paulrobinson/mtb/data/WeatherResult2.java
@@ -11,16 +11,16 @@ public class WeatherResult2 {
     public String locationName;
     public String postcode;
     public Double latitude;
-    public Double longtitde;
+    public Double longitude;
 
-    public List<ForcastData.Feature.Properties.FutureWeatherData> futureWeatherData = new ArrayList<>();
     public List<RainfallData.PastWeatherData> pastWeatherData = new ArrayList<>();
 
+    public List<ForcastData.Feature.Properties.FutureWeatherData> futureWeatherData = new ArrayList<>();
 
-    public WeatherResult2(String locationName, String postcode, Double latitude, Double longtitde) {
+    public WeatherResult2(String locationName, String postcode, Double latitude, Double longitude) {
         this.locationName = locationName;
         this.postcode = postcode;
         this.latitude = latitude;
-        this.longtitde = longtitde;
+        this.longitude = longitude;
     }
 }
--- src/main/java/eu/paulrobinson/mtb/loader/DataLoader.java
+++ src/main/java/eu/paulrobinson/mtb/loader/DataLoader.java
@@ -1,6 +1,5 @@
 package eu.paulrobinson.mtb.loader;
 
-import eu.paulrobinson.mtb.data.WeatherResult;
 import eu.paulrobinson.mtb.data.WeatherResult2;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -25,6 +24,12 @@ public class DataLoader {
 
     private List<WeatherResult2> weatherResults = new ArrayList<>();
 
+    public DataLoader() {
+
+        WeatherResult2 weatherResult = new WeatherResult2("Hamsterley Forrest", "DL13 3NL", 54.6586, -1.9325);
+        weatherResults.add(weatherResult);
+    }
+
     public List<WeatherResult2> getWeatherResults() {
 
         refreshDate();
@@ -34,35 +39,54 @@ public class DataLoader {
 
     public void refreshDate() {
 
-        LocalDate current =  LocalDate.now();
-        RainfallData rainfallData = rainfallService.getRainfallData(current.minusDays(pastDataDays).toString(), "rainfall", true);
+        loadPastWeather();
+        loadFutureWeather();
+    }
 
+    private void loadPastWeather() {
+        LocalDate current =  LocalDate.now();
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/London"));
 
-        WeatherResult2 weatherResult = new WeatherResult2("Hamsterley Forrest", "DL13 3NL", 54.6586, -1.9325);
+        for (WeatherResult2 weatherResult : weatherResults) {
 
-        Map<Integer, RainfallData.PastWeatherData> readings = new HashMap<>();
-        for (RainfallData.PastWeatherData pastWeatherData : rainfallData.items) {
+            RainfallData rainfallData = rainfallService.getRainfallData(current.minusDays(pastDataDays).toString(), "rainfall", true);
+            Map<Integer, RainfallData.PastWeatherData> readings = new HashMap<>();
+            for (RainfallData.PastWeatherData pastWeatherData : rainfallData.items) {
 
-            cal.setTime(pastWeatherData.dateTime);
-            Integer dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+                cal.setTime(pastWeatherData.dateTime);
+                Integer dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
 
-            RainfallData.PastWeatherData reading = readings.get(dayOfMonth);
-            if (reading == null) {
-                reading = new RainfallData.PastWeatherData();
-                readings.put(dayOfMonth, reading);
-                reading.dateTime = pastWeatherData.dateTime;
+                RainfallData.PastWeatherData reading = readings.get(dayOfMonth);
+                if (reading == null) {
+                    reading = new RainfallData.PastWeatherData();
+                    readings.put(dayOfMonth, reading);
+                    reading.dateTime = pastWeatherData.dateTime;
+                }
+
+                reading.value += pastWeatherData.value;
             }
 
-            reading.value += pastWeatherData.value;
+            List<RainfallData.PastWeatherData> readingsResults = new ArrayList<>(readings.values());
+            Collections.sort(readingsResults);
+
+            weatherResult.pastWeatherData = readingsResults;
         }
+    }
 
-        List<RainfallData.PastWeatherData> readingsResults = new ArrayList<>(readings.values());
-        Collections.sort(readingsResults);
+    private void loadFutureWeather() {
 
-        weatherResult.pastWeatherData = readingsResults;
+        for (WeatherResult2 weatherResult : weatherResults) {
+            ForcastData forcastData = forcastService.forcastDataThreeHourly(false, weatherResult.latitude, weatherResult.longitude, "a4dc9522-17c6-460f-8096-c6f757203eeb", "bU6tF0cC3lV5bP5eN1cB4hA5pM6bA1jM6lF4aW1uR7lA6eS6cQ");
 
-        weatherResults.add(weatherResult);
+            Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/London"));
+            for (ForcastData.Feature.Properties.FutureWeatherData data : forcastData.getData()) {
+                cal.setTime(data.time);
+                if (cal.get(Calendar.HOUR_OF_DAY) == 18) {
+                    System.out.println(data.time);
+                    weatherResult.futureWeatherData.add(data);
+                }
+            }
+        }
 
     }
 }
